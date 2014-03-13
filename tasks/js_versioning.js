5f395d117eb74cd9391517dbdc82480294d5e58d
Robert Petz <rpetz@petzent.com>
2014-03-13 15:02:12 -0700

    Finalized basic implementation, need to still add documentation

--- tasks/js_versioning.js
+++ tasks/js_versioning.js
@@ -10,44 +10,43 @@
 
 'use strict';
 
-var path = require('path');
-
-/* From grunt-contrib-uglify */
-// Return the relative path from file1 => file2
-var relativePath = function(file1, file2) {
-	var file1Dirname = path.dirname(file1);
-	var file2Dirname = path.dirname(file2);
-	if (file1Dirname !== file2Dirname)
-		return path.relative(file1Dirname, file2Dirname) + path.sep;
-	return "";
-};
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
 
 module.exports = function(grunt) {
-	grunt.registerMultiTask('version', 'A plugin for bumping a version number in a javascript file for your application', function() {
+	grunt.registerMultiTask('jsversion', 'A plugin for bumping a version number in a javascript file for your application', function() {
+		grunt.log.writeln('Versioning output file')
+
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
 			majorVersion: '',
 			minorVersion: '',
+			versionFile: '',
+			outputFile: ''
 		});
 
-		var source = this.versionFile;
-		var output = this.outputFile;
+		grunt.log.writeln('Version file: ' + options.versionFile);
+		grunt.log.writeln('Output file: ' + options.outputFile);
 
-		if (!grunt.file.exists(source))
+		if (!grunt.file.exists(options.versionFile))
 		{
-			grunt.log.warn('Versioning file ' + chalk.cyan(source) + ' not found.');
+			grunt.log.warn('Versioning file ' + chalk.cyan(options.versionFile) + ' not found.');
 			return;
 		}
 
-		if (!grunt.file.exists(output))
+		if (!grunt.file.exists(options.outputFile))
 		{
-			grunt.log.warn('Output file ' + chalk.cyan(source) + ' not found.');
+			grunt.log.warn('Output file ' + chalk.cyan(options.versionFile) + ' not found.');
 			return;
 		}
 
-		var sourceContent = grunt.file.read(source);
+		var sourceContent = grunt.file.read(options.versionFile);
 		if (sourceContent.length == 0) sourceContent = "0";
 		var buildVersion = parseInt(sourceContent);
+		grunt.log.writeln('Old Version: ' + buildVersion);
 
 		if (isNaN(buildVersion))
 		{
@@ -55,18 +54,34 @@ module.exports = function(grunt) {
 			return;
 		}
 
-		buildVersion++;
-		var newVersion = majorVersion + "." + minorVersion + "." + buildVersion;
+		buildVersion = buildVersion + 1;
+		var newVersion = options.majorVersion + "." + options.minorVersion + "." + buildVersion;
 
 		grunt.log.writeln('Writing new version: ' + newVersion);
-		grunt.file.write(source, buildVersion);
+		grunt.file.write(options.versionFile, buildVersion);
 
-		var destinationContent = grunt.file.read(output);
+		var d = new Date();
+		var h = d.getHours();
+		var isPM = false;
+		if (h > 12) 
+		{
+			h = h - 12;
+			isPM = true;
+		}
+
+		h = pad(h, 2);
+		var s = d.getSeconds();
+		s = pad(s, 2);
+		var m = d.getMinutes();
+		m = pad(m, 2);
+		var timestamp = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + " " + h + ":" + m + "." + s + " " + (isPM ? "PM" : "AM");
+
+		var destinationContent = grunt.file.read(options.outputFile);
 		destinationContent = destinationContent.replace(/<!version--!>/g, newVersion)
-		destinationContent = destinationContent.replace(/<!timestamp--!>/g, (new Date()).toLocaleString());
+		destinationContent = destinationContent.replace(/<!timestamp--!>/g, timestamp);
 
 		grunt.log.writeln('Performing output replacements');
-		grunt.file.write(output, destinationContent);
+		grunt.file.write(options.outputFile, destinationContent);
 
 		grunt.log.writeln('Complete');
 	});
